306183031ee7a0db727fa8795bc61e8e9ae6ea59
ImenHzl <166798845+ImenHzl@users.noreply.github.com>
2024-05-25 23:56:08 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -77,7 +77,7 @@ def enregistrer_client():
     conn.close()
     return redirect('/consultation/')  # Rediriger vers la page d'accueil après l'enregistrement
 
-@app.route('/fiche_nom/')
+@app.route('/fiche_nom/', methods=['GET', 'POST'])
 def ReadficheNom():
     nom = request.form['name']
     conn = sqlite3.connect('database.db')
