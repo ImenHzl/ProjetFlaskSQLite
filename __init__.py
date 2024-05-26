5dfeac5d15df48448689bdeb309e0b49a14b754d
ImenHzl <166798845+ImenHzl@users.noreply.github.com>
2024-05-26 16:48:56 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -80,6 +80,7 @@ def enregistrer_client():
 @app.route('/fiche_nom/', methods=['GET', 'POST'])
 def ReadficheNom():
     data = None
+    if request.method == 'POST':
         nom = request.form['name']
         conn = sqlite3.connect('database.db')
         cursor = conn.cursor()
